bf4c4d98b31e40b39839d4196279d22c40d941a7
kozlovmv <kozlov.mvl@yandex.ru>
2018-11-14 17:49:43 +0300

    v0.1 - добавлен статические метод вычисления определителя матрицы; - добавлен статический метод проверки критерия Сильвестра.

--- test.py
+++ test.py
@@ -8,5 +8,8 @@ from tools import Form
 
 if __name__ == '__main__':
     # create object form
-    form = Form(dim=3, deg=4)
-    print(form.get_matching())
+    # form = Form(dim=3, deg=4)
+    # matrix = form.is_positive()
+    # for line in matrix:
+    #     print(line)
+    print(Form.get_det([[1,2,3], [4,5,6], [7,8,9]]))
--- tools.py
+++ tools.py
@@ -48,7 +48,31 @@ class Form(object):
         gen(dim, deg)
         return list_monoms
 
-    def get_matching(self):
+    @staticmethod
+    def get_det(matrix):
+        if len(matrix) == 2:
+            return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
+        temp = 0
+        for i, line in enumerate(matrix):
+            temp += (-1)**i*line[0]*Form.get_det([line_[1:] for k, line_ in enumerate(matrix) if k != i])
+        return temp
+
+    @staticmethod
+    def silvestr(matrix):
+        if any([line[i] <= 0 for i, line in enumerate(matrix)]):
+            return False
+        det = matrix[0][0]
+        for row in range(1, len(matrix)):
+            temp = 0
+            for j in range(row):
+                temp += (-1)**(row+j)*Form.get_det([line[:j]+line[j+1:] for line in matrix[:row]])
+            temp += matrix[row][row]*det
+            if temp <= 0:
+                return False
+            det = temp
+        return True
+
+    def __get_matching(self):
         old_monoms = Form.gen_monoms(self.dim, self.deg)
         indexes_old_monoms = {mon: i for i, mon in enumerate(old_monoms)}
         young_monoms = Form.gen_monoms(self.dim, self.deg // 2)
@@ -60,11 +84,42 @@ class Form(object):
                     matching[indexes_old_monoms[old_monom]].append((i, j))
                 except KeyError:
                     matching[indexes_old_monoms[old_monom]] = [(i, j)]
-        return matching
+        num_add = 0
+        for item in matching.values():
+            if len(item) == 2:
+                num_add += 1
+        return matching, num_add
 
-    def is_positive(self):
+    def __get_q_matrix(self, matching):
+        q_matrix = [[0 for i in range(self.q_dim)] for j in range(self.q_dim)]
+        for old_index, couples in matching.items():
+            q_matrix[couples[0][0]][couples[0][1]] = self.coeffs[old_index]
+        return q_matrix
+
+    def __get_full_matrix(self, matching, args):
+        assert len(args) == self.num_add, ('Length of \'args\' must be equal %s' % self.num_add)
+        full_matrix = [[0 for i in range(self.q_dim)] for j in range(self.q_dim)]
+        count = 0
+        for old_index, couples in matching.items():
+            full_matrix[couples[0][0]][couples[0][1]] = self.coeffs[old_index] / 2
+            full_matrix[couples[0][1]][couples[0][0]] = self.coeffs[old_index] / 2
+            if len(couples) == 2:
+                full_matrix[couples[0][0]][couples[0][1]] = args[count] / 2
+                full_matrix[couples[0][1]][couples[0][0]] = args[count] / 2
+                full_matrix[couples[1][0]][couples[1][1]] = -args[count] / 2
+                full_matrix[couples[1][1]][couples[1][0]] = -args[count] / 2
+                count += 1
+        return full_matrix
+
+    def is_positive(self, radius=1):
         assert not self.deg % 2, ('Degree must be even number')
+        assert isinstance(radius, int) or isinstance(radius, float), ('Argument \'radius\' must be '
+                                                                      'integer or float number')
+        assert radius > 0, ('Argument \'radius\' must be positive')
+        self.q_dim = Form.binom(self.dim + self.deg // 2 - 1, self.dim - 1)
         sign = False
         # превращаем в квадратичные формы
-
-        return bool(sign)
+        matching, self.num_add = self.__get_matching()
+        self.area = [[-radius, radius] for i in range(self.num_add)]
+        full_matrix = self.__get_full_matrix(matching, [1,1,1,1,1,1])
+        return full_matrix
