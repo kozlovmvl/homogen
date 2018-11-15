e77908a31d18df3b7ffaceaabd33ce7899a60212
kozlovmv <kozlov.mvl@yandex.ru>
2018-11-15 14:56:19 +0300

    v0.1 - добавлено unit-тестирование.

--- test.py
+++ test.py
@@ -2,11 +2,24 @@
 Testing
 """
 
+import json
+import unittest
 
 from tools import Form
 
 
+class TestForm(unittest.TestCase):
+
+    def test_is_positive(self):
+        with open('forms.json', 'r') as f:
+            data = json.loads(f.read())
+        results = []
+        for item in data:
+            form = Form(dim=item['dim'], deg=item['deg'], coeffs=item['coeffs'])
+            results.append(form.is_positive() is item['is_positive'])
+        self.assertTrue(all(results))
+
+
 if __name__ == '__main__':
-    # create object form
-    form = Form(dim=2, deg=4, coeffs=[1, 0, 2, 0, 2])
-    print(form.is_positive(radius=2))
+    unittest.main()
+    # print(Form.gen_monoms(2, 6))
